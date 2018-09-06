c95e1af56e259a8453eaec0baa077cc1151d024f
stark10war <shashank.tanwar32@gmail.com>
2018-09-07 01:54:15 +0530

    K means Clustering done. Found the appropriate No of Clusters

--- K- Means Clustering -Airlines Cluster.R
+++ K- Means Clustering -Airlines Cluster.R
@@ -1,20 +1,66 @@
 
 #======================Clustring Project- Airlines Cluster By SHASHANK TANWAR=========================================
 
+#Setting up Enviromnent for Clustering
 
+list.of.packages <- c("datasets", "ggplot2","cluster")
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages, repos="http://cran.rstudio.com/")
+install.packages("cluster", dependencies = TRUE)
 
+library(cluster)
+library(ggplot2)
 
+#Importing Dataset
+getwd()
 
+Airlines<- read.csv("AirlinesCluster.csv")
+str(Airlines)
+dim(Airlines)
+summary(Airlines)
 
+# Checking Null Values
 
+colSums(is.na(Airlines))
 
+# Standarising the dataset
+install.packages("standardize")
+library(standardize)
 
+Airlines.scaled<- scale(Airlines, center = TRUE, scale = TRUE)
+summary(Airlines.scaled)
 
+# K-means Clustering
+library(caret)
+set.seed(24)
 
+# geting within sum of squares for different no of clusters.
+WSS<- list() 
+K <- 1:20
+for (i in K) {
+  kmeans(Airlines.scaled, i,iter.max = 1000)
+  WSS<- append(WSS,values = sum(kmeans(Airlines.scaled,i, iter.max = 1000)$withinss))
+}
 
+#WSS dataframe
 
+WSSdata<- cbind(K, WSS)
+  
+WSSdata<-  as.data.frame(WSSdata)
 
+plot(WSSdata)
 
+# Hence the required no of clusters can be 5 or 6 clusters.
 
+K= 5
 
+AirlinesCluster<- kmeans(Airlines.scaled, K, iter.max = 1000)
+summary(AirlinesCluster)
 
+AirlinesCluster$withinss
+table(AirlinesCluster$cluster)
+AirlinesCluster$centers
+
+#Adding Cluster column in the data frame
+Airlines$Cluster<- AirlinesCluster$cluster
+write.csv(Airlines, "ClusterResults.csv")
