bc88c2ed72288098891d9290fa549fe787aa9b36
stark10war <shashank.tanwar32@gmail.com>
2018-09-08 01:36:45 +0530

    Hierarchical clustring codes added. Dendrogram image added. hierarchical clustering result and cluster means summary csv files added.

--- K- Means Clustering -Airlines Cluster.R
+++ K- Means Clustering -Airlines Cluster.R
@@ -60,7 +60,7 @@ AirlinesCluster$centers
 
 #Adding Cluster column in the data frame
 Airlines$Cluster<- AirlinesCluster$cluster
-write.csv(Airlines, "ClusterResults.csv")
+write.csv(Airlines, "K-means ClusterResults.csv")
 
 #Cluster summary
 library(dplyr)
@@ -70,7 +70,33 @@ Cluster_means<- summarise(group_by(Airlines, Cluster), Balance = mean(Balance, n
                           BonusTrans= mean(BonusTrans, na.rm = TRUE),FlightMiles= mean(FlightMiles, na.rm = TRUE),
                           FlightTrans = mean(FlightTrans, na.rm = TRUE), DaysSinceEnroll = mean(DaysSinceEnroll, na.rm = TRUE))
 
-write.csv(Cluster_means, "Cluster Means.csv")
+write.csv(Cluster_means, "K-means Cluster Means summary.csv")
+
+#===================Creating Hierarchical Clustering=====================================
+
+
+#Hierarchical Clustering
+
+Airlines$Cluster<- NULL 
+distances<- dist(Airlines,method = "euclidean")
+AirlinesHcluster<- hclust(distances, method = "ward.D")
+plot(AirlinesHcluster)
+
+# getting clusters using cutree
+Hclusters<-cutree(AirlinesHcluster, k=5)
+table(Hclusters)
+
+# Creating Hcluster column
+Airlines$hcluster<- Hclusters
+write.csv(Airlines, "Hierarchical Cluster Result.csv")
+#Creating cluster means summary table
+
+Hcluster_means<- summarise(group_by(Airlines, hcluster), Balance = mean(Balance, na.rm = TRUE), 
+                          QualMiles= mean(QualMiles, na.rm = TRUE),BonusMiles= mean(BonusMiles, na.rm = TRUE),
+                          BonusTrans= mean(BonusTrans, na.rm = TRUE),FlightMiles= mean(FlightMiles, na.rm = TRUE),
+                          FlightTrans = mean(FlightTrans, na.rm = TRUE), DaysSinceEnroll = mean(DaysSinceEnroll, na.rm = TRUE))
+
+write.csv(Hcluster_means, "Hierarchical Clusters mean summary.csv")
 
 
 
