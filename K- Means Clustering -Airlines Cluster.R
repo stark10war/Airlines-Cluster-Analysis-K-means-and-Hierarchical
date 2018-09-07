decdd1fd16393b447153c6a878d6d98d92ce96d6
stark10war <shashank.tanwar32@gmail.com>
2018-09-08 00:42:33 +0530

    Cluster means summary codes added

--- K- Means Clustering -Airlines Cluster.R
+++ K- Means Clustering -Airlines Cluster.R
@@ -6,11 +6,9 @@
 list.of.packages <- c("datasets", "ggplot2","cluster")
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages, repos="http://cran.rstudio.com/")
-install.packages("cluster", dependencies = TRUE)
-
 library(cluster)
 library(ggplot2)
-
+library(caret)
 #Importing Dataset
 getwd()
 
@@ -24,15 +22,13 @@ summary(Airlines)
 colSums(is.na(Airlines))
 
 # Standarising the dataset
-install.packages("standardize")
 library(standardize)
 
 Airlines.scaled<- scale(Airlines, center = TRUE, scale = TRUE)
 summary(Airlines.scaled)
 
 # K-means Clustering
-library(caret)
-set.seed(24)
+set.seed(88)
 
 # geting within sum of squares for different no of clusters.
 WSS<- list() 
@@ -41,6 +37,7 @@ for (i in K) {
   kmeans(Airlines.scaled, i,iter.max = 1000)
   WSS<- append(WSS,values = sum(kmeans(Airlines.scaled,i, iter.max = 1000)$withinss))
 }
+WSS
 
 #WSS dataframe
 
@@ -64,3 +61,16 @@ AirlinesCluster$centers
 #Adding Cluster column in the data frame
 Airlines$Cluster<- AirlinesCluster$cluster
 write.csv(Airlines, "ClusterResults.csv")
+
+#Cluster summary
+library(dplyr)
+colnames(Airlines)
+Cluster_means<- summarise(group_by(Airlines, Cluster), Balance = mean(Balance, na.rm = TRUE), 
+                          QualMiles= mean(QualMiles, na.rm = TRUE),BonusMiles= mean(BonusMiles, na.rm = TRUE),
+                          BonusTrans= mean(BonusTrans, na.rm = TRUE),FlightMiles= mean(FlightMiles, na.rm = TRUE),
+                          FlightTrans = mean(FlightTrans, na.rm = TRUE), DaysSinceEnroll = mean(DaysSinceEnroll, na.rm = TRUE))
+
+write.csv(Cluster_means, "K-means Clusters Summary.csv")
+
+
+
